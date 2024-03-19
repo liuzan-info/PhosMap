58673f55586cdb85b2940185fe52998a9521e819
liuzan-info <liuzan_info@163.com>
2024-03-19 16:33:30 +0800

    solve conflict

--- r_packages_install.R
+++ r_packages_install.R
@@ -4,4 +4,9 @@ if (!require("BiocManager", quietly = TRUE)){install.packages("BiocManager")}
 
 BiocManager::install(c("pcaMethods", "impute"))
 
-install.packages(c("FactoMineR","factoextra","data.table","tidyr","shiny","shinyjs","shinyBS","shinyWidgets","ggplot2","ggrepel","plotly","colourpicker","ggseqlogo","pheatmap","survminer","survival","zip","stringr","dplyr","DT","png","svglite","ggplotify","bslib","qpdf", "rrcovNA", "e1071", "heatmaply", "ggdendro"))
+# due to FactoMineR's update, dependency conflict occurs
+install.packages("estimability_1.4.1.tar.gz", type="Source", repos=NULL)
+install.packages("emmeans_1.8.9.tar.gz", type="Source", repos=NULL)
+install.packages("FactoMineR_2.9.tar.gz", type="Source", repos=NULL)
+
+install.packages(c("factoextra","data.table","tidyr","shiny","shinyjs","shinyBS","shinyWidgets","ggplot2","ggrepel","plotly","colourpicker","ggseqlogo","pheatmap","survminer","survival","zip","stringr","dplyr","DT","png","svglite","ggplotify","bslib","qpdf", "rrcovNA", "e1071", "heatmaply", "ggdendro"))
